687a30efa2c31b672646ca41a93d779b3e3e5fa0
kodziosek <57525477+kodziosek@users.noreply.github.com>
2022-03-03 14:43:32 +0100

    03.03_15:00_gitrepair

---
+++ ProjektKarolewski/Entities/Reply.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektKarolewski.Entities
+{
+    public class Reply
+    {
+        public int Id { get; set; }
+        public DateTime ReplyDate { get; set; }
+        public string ReplyDescription { get; set; }
+        public string ReplyProtocol { get; set; }
+
+        public virtual Ticket Ticket { get; set; }
+    }
+}
